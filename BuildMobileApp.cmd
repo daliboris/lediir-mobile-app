462b39685b992d26372e74a03b1fca739fba07f6
Boris Lehečka <boris@daliboris.cz>
2023-11-24 00:35:08 +0100

    setting full path to dictionary; preventing deletion of primary data

--- BuildMobileApp.cmd
+++ BuildMobileApp.cmd
@@ -1,1 +1,2 @@
-call "C:\Program Files (x86)\SIL\Dictionary App Builder\dab.bat" -load "Persian-Czech Dictionary" -build
+cd dictionaries
+call "C:\Program Files (x86)\SIL\Dictionary App Builder\dab.bat" -load "V:\Projekty\Github\Daliboris\lediir-mobile-app\dictionaries\FACS\FACS" -build -no-save
